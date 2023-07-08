c8faedf4d3206d9efffd625b3b6982aa2c41559f
luhurfatah <luhurfatah@gmail.com>
2023-07-08 22:46:52 +0700

    add disk provisoning automation

--- disks/main.tf
+++ disks/main.tf
@@ -5,58 +5,105 @@ provider "google" {
   zone        = "us-east1-b"
 }
 
+resource "google_compute_disk" "openstack01_os" {
+  name  = "openstack01-os"
+  type  = "pd-balanced"
+  zone  = "us-central1-b"
+  size  = "10"
+  image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20230630"
+}
+
+resource "google_compute_disk" "openstack02_os" {
+  name  = "openstack02-os"
+  type  = "pd-balanced"
+  zone  = "us-central1-b"
+  size  = "10"
+  image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20230630"
+}
+
+resource "google_compute_disk" "ceph01_os" {
+  name  = "ceph01-os"
+  type  = "pd-balanced"
+  zone  = "us-east1-b"
+  size  = "10"
+  image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20230630"
+}
 resource "google_compute_disk" "ceph01_disk1" {
   name = "ceph01-disk1"
   type = "pd-ssd"
   zone = "us-east1-b"
-  size = "25"
+  size = "10"
 }
 
 resource "google_compute_disk" "ceph01_disk2" {
   name = "ceph01-disk2"
   type = "pd-ssd"
   zone = "us-east1-b"
-  size = "25"
+  size = "10"
+}
+
+resource "google_compute_disk" "ceph02_os" {
+  name  = "ceph02-os"
+  type  = "pd-balanced"
+  zone  = "us-west4-b"
+  size  = "10"
+  image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20230630"
 }
 
 resource "google_compute_disk" "ceph02_disk1" {
   name = "ceph02-disk1"
   type = "pd-ssd"
-  zone = "us-east1-b"
-  size = "25"
+  zone = "us-west4-b"
+  size = "10"
 }
 
 resource "google_compute_disk" "ceph02_disk2" {
   name = "ceph02-disk2"
   type = "pd-ssd"
-  zone = "us-east1-b"
-  size = "25"
+  zone = "us-west4-b"
+  size = "10"
 }
+
+resource "google_compute_disk" "glusterfs01_os" {
+  name  = "glusterfs01-os"
+  type  = "pd-balanced"
+  zone  = "us-east1-b"
+  size  = "10"
+  image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20230630"
+}
+
 
 resource "google_compute_disk" "glusterfs01_disk1" {
   name = "glusterfs01-disk1"
   type = "pd-ssd"
   zone = "us-east1-b"
-  size = "25"
+  size = "10"
 }
 
 resource "google_compute_disk" "glusterfs01_disk2" {
   name = "glusterfs01-disk2"
   type = "pd-ssd"
   zone = "us-east1-b"
-  size = "25"
+  size = "10"
 }
 
+resource "google_compute_disk" "glusterfs02_os" {
+  name  = "glusterfs02-os"
+  type  = "pd-balanced"
+  zone  = "us-west4-b"
+  size  = "10"
+  image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20230630"
+}
 resource "google_compute_disk" "glusterfs02_disk1" {
   name = "glusterfs02-disk1"
   type = "pd-ssd"
-  zone = "us-east1-b"
-  size = "25"
+  zone = "us-west4-b"
+  size = "10"
 }
 
 resource "google_compute_disk" "glusterfs02_disk2" {
   name = "glusterfs02-disk2"
   type = "pd-ssd"
-  zone = "us-east1-b"
-  size = "25"
+  zone = "us-west4-b"
+  size = "10"
 }
